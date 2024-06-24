813187f363bf135787f374c526a08a01379e367e
mouadse <mouadse@gmail.com>
2024-06-24 21:59:19 +0100

    Back to shipping

--- so_long.c
+++ so_long.c
@@ -2,7 +2,5 @@
 
 int main(void) {
   printf("Hello World!!!\n");
-  int age = 123;
-  printf("The age value is %d\n", age);
   return (EXIT_SUCCESS);
 }
--- so_long.h
+++ so_long.h
@@ -1,81 +1,75 @@
 #ifndef SO_LONG_H
-# define SO_LONG_H
+#define SO_LONG_H
+#include "./mlx/mlx.h"
 
-# include <assert.h>
-# include <errno.h>
-# include <fcntl.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-# define ESC_KEY 53
-# define UP_KEY_W 13
-# define UP_KEY_ARROW 126
-# define LEFT_KEY_A 0
-# define LEFT_KEY_ARROW 123
-# define DOWN_KEY_S 1
-# define DOWN_KEY_ARROW 125
-# define RIGHT_KEY_D 2
-# define RIGHT_KEY_ARROW 124
+#define ESC_KEY 53
+#define UP_KEY_W 13
+#define UP_KEY_ARROW 126
+#define LEFT_KEY_A 0
+#define LEFT_KEY_ARROW 123
+#define DOWN_KEY_S 1
+#define DOWN_KEY_ARROW 125
+#define RIGHT_KEY_D 2
+#define RIGHT_KEY_ARROW 124
 
-typedef struct s_character
-{
-	int						x_pos;
-	int						y_pos;
-	int						collected_coins;
-	int						moves_count;
-	void					*spirites[4];
-	void					*curr_animation;
-	int						facing_direction;
-}							t_character;
+typedef struct s_character {
+  int col;
+  int row;
+  int collected_coins;
+  int moves_count;
+  void *spirites[4];
+  void *curr_animation;
+  int facing_direction;
+} t_character;
 
-typedef struct s_lvl_exit
-{
-	int						x_pos;
-	int						y_pos;
-}							t_lvl_exit;
+typedef struct s_lvl_exit {
+  int col;
+  int row;
+} t_lvl_exit;
 
-typedef struct s_game_assets
-{
-	void					*wall_asset;
-	void					*coin_asset;
-	void					*floor_asset;
-	void					*closed_door_asset;
-	void					*open_door_asset;
-}							t_game_assets;
+typedef struct s_game_assets {
+  void *wall_asset;
+  void *coin_asset;
+  void *floor_asset;
+  void *closed_door_asset;
+  void *open_door_asset;
+} t_game_assets;
 
-typedef struct s_game_map
-{
-	int						height;
-	int						width;
-	char					*file_path;
-	char					**layout;
-	int						total_collectibles;
-	t_lvl_exit				exit_position;
-}							t_game_map;
+typedef struct s_game_map {
+  int height;
+  int width;
+  char *file_path;
+  char **layout;
+  int total_collectibles;
+  t_lvl_exit exit_position;
+} t_game_map;
 
-struct						s_map_line_node
-{
-	char					*line_content;
-	struct s_map_line_node	*next_line;
+struct s_map_line_node {
+  char *line_content;
+  struct s_map_line_node *next_line;
 };
 
-typedef struct s_map_parse_queue
-{
-	struct s_map_line_node	*first_line;
-	struct s_map_line_node	*last_line;
-}							t_map_parse_queue;
+typedef struct s_map_parse_queue {
+  struct s_map_line_node *first_line;
+  struct s_map_line_node *last_line;
+} t_map_parse_queue;
 
-typedef struct s_game_state
-{
-	void					*mlx_instance;
-	void					*window_instance;
-	t_game_map				map;
-	t_map_parse_queue		parse_queue;
-	t_character				player;
-	t_lvl_exit				level_exit;
-	t_game_assets			assets;
-	int						game_completed;
-}							t_game_state;
+typedef struct s_game_state {
+  void *mlx_instance;
+  void *window_instance;
+  t_game_map map;
+  t_map_parse_queue parse_queue;
+  t_character player;
+  t_lvl_exit level_exit;
+  t_game_assets assets;
+  int game_completed;
+} t_game_state;
 
 #endif
