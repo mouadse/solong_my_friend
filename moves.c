184bc6783abd9b492a5c157f6bb2850eb732f173
mouadse <mouadse@gmail.com>
2024-06-26 00:27:04 +0100

    X

--- moves.c
+++ moves.c
@@ -28,3 +28,60 @@ void move_sprite_up(t_game_state *state) {
            state->player.moves_count); // to be refactored later with own printf
   }
 }
+
+void move_sprite_down(t_game_state *state) {
+  if (valid_move(state, state->player.row + 1, state->player.col)) {
+    state->player.row += 1;
+    state->player.moves_count += 1;
+    state->player.facing_direction = 2;
+    if (state->map.layout[state->player.row][state->player.col] == 'C') {
+      state->player.score += 10;
+      state->map.layout[state->player.row][state->player.col] = '2';
+    }
+    mlx_put_image_to_window(state->mlx_instance, state->window_instance,
+                            state->player.curr_animation,
+                            state->player.col * 32, state->player.row * 32);
+    if (state->player.score == state->map.total_collectibles * 10)
+      state->game_completed = 1;
+    printf("Moves %d\n",
+           state->player.moves_count); // to be refactored later with own printf
+  }
+}
+
+void move_sprite_right(t_game_state *state) {
+  if (valid_move(state, state->player.row, state->player.col + 1)) {
+    state->player.col += 1;
+    state->player.moves_count += 1;
+    state->player.facing_direction = 3;
+    if (state->map.layout[state->player.row][state->player.col] == 'C') {
+      state->player.score += 10;
+      state->map.layout[state->player.row][state->player.col] = '2';
+    }
+    mlx_put_image_to_window(state->mlx_instance, state->window_instance,
+                            state->player.curr_animation,
+                            state->player.col * 32, state->player.row * 32);
+    if (state->player.score == state->map.total_collectibles * 10)
+      state->game_completed = 1;
+    printf("Moves %d\n",
+           state->player.moves_count); // to be refactored later with own printf
+  }
+}
+
+void move_sprite_left(t_game_state *state) {
+  if (valid_move(state, state->player.row, state->player.col - 1)) {
+    state->player.col -= 1;
+    state->player.moves_count += 1;
+    state->player.facing_direction = 1;
+    if (state->map.layout[state->player.row][state->player.col] == 'C') {
+      state->player.score += 10;
+      state->map.layout[state->player.row][state->player.col] = '2';
+    }
+    mlx_put_image_to_window(state->mlx_instance, state->window_instance,
+                            state->player.curr_animation,
+                            state->player.col * 32, state->player.row * 32);
+    if (state->player.score == state->map.total_collectibles * 10)
+      state->game_completed = 1;
+    printf("Moves %d\n",
+           state->player.moves_count); // to be refactored later with own printf
+  }
+}
