9a174659073a22fb8d323bbb8086601577e87015
mouadse <mouadse@gmail.com>
2024-06-26 20:04:53 +0100

    adding util functions to validate our map contenty

--- map_utils.c
+++ map_utils.c
@@ -30,7 +30,7 @@ int	is_row_all_walls(const char *row)
 	return (1);
 }
 
-int	count_map_characters(t_game_map *map, char target_char)
+int	count_map_characters(const t_game_map *map, char target_char)
 {
 	int	count;
 	int	row;
@@ -72,6 +72,36 @@ int	validate_map_format(const t_game_map *map)
 			col++;
 		}
 		row++;
+	}
+	return (1);
+}
+
+int	validate_map(const t_game_map *map)
+{
+	int	num_exits;
+	int	num_collectibles;
+	int	num_starting_positions;
+
+	num_exits = count_map_characters(map, 'E');
+	num_collectibles = count_map_characters(map, 'C');
+	num_starting_positions = count_map_characters(map, 'P');
+	if (num_exits != 1)
+	{
+		// printf should be replaced by own function
+		printf("Error\n❌ The map must contain exactly one exit point ❌\n");
+		return (0);
+	}
+	if (num_collectibles < 1)
+	{
+		// printf should be replaced by own function
+		printf("Error\n❌ The map must contain at least one collectible ❌\n");
+		return (0);
+	}
+	if (num_starting_positions != 1)
+	{
+		// printf should be replaced by own function
+		printf("Map must contain exactly one starting position ❌\n");
+		return (0);
 	}
 	return (1);
 }
