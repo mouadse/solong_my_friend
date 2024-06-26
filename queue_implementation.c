c6da33fc9d3c0795b676901b6cda38fc568ea314
mouadse <mouadse@gmail.com>
2024-06-26 16:34:43 +0100

    Adding helpful funcs to queues

--- queue_implementation.c
+++ queue_implementation.c
@@ -1,1 +1,40 @@
 #include "so_long.h"
+#include <cstddef>
+
+void initialize_queue(t_map_parse_queue *queue) {
+  queue->first_line = NULL;
+  queue->last_line = NULL;
+}
+
+int is_queue_empty(t_map_parse_queue *q) { return (q->first_line == NULL); }
+
+void add_to_queue(t_map_parse_queue *queue, char *line) {
+  t_map_line_node *node;
+
+  node = malloc(sizeof(t_map_line_node));
+  if (!node)
+    return;
+  node->line_content = line;
+  node->next_line = NULL;
+  if (is_queue_empty(queue)) {
+    queue->first_line = node;
+    queue->last_line = node;
+  } else {
+    queue->last_line->next_line = node;
+    queue->last_line = node;
+  }
+}
+
+char *remove_from_queue(t_map_parse_queue *queue) {
+  t_map_line_node *temp;
+  char *line;
+  if (is_queue_empty(queue))
+    return (NULL);
+  temp = queue->first_line;
+  line = temp->line_content;
+  queue->first_line = queue->first_line->next_line;
+  free(temp);
+  if (!queue->first_line)
+    queue->last_line = NULL;
+  return (line);
+}
