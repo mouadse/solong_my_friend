b7d6149713f190503af12348dfaf985628128512
mouadse <mouadse@gmail.com>
2024-06-26 03:33:07 +0100

    Fixing render issues and adding exits and NPC location

--- player.c
+++ player.c
@@ -1,3 +1,4 @@
+#include "mlx/mlx.h"
 #include "so_long.h"
 
 void initialize_and_load_player_images(t_game_state *state) {
@@ -19,4 +20,32 @@ void initialize_and_load_player_images(t_game_state *state) {
       state->mlx_instance, "./textures/player/pac_man3.xpm", &width, &height);
   state->player.curr_animation = mlx_xpm_file_to_image(
       state->mlx_instance, "./textures/player/animation.xpm", &width, &height);
+}
+
+void render_player(t_game_state *state) {
+  mlx_put_image_to_window(
+      state->mlx_instance, state->window_instance,
+      state->player.spirites[state->player.facing_direction],
+      state->player.col * 32, state->player.row * 32);
+}
+
+void locate_player_and_exit_positions(t_game_state *state) {
+  int i;
+  int j;
+
+  for (i = 0; i < state->map.rows; i++) {
+    for (j = 0; j < state->map.cols; j++) {
+      if (state->map.layout[i][j] == 'P') {
+        state->player.col = j;
+        state->player.row = i;
+      } else if (state->map.layout[i][j] == 'E') {
+        state->level_exit.col = j;
+        state->level_exit.row = i;
+      }
+    }
+  }
+
+  // Set the target position to the exit position
+  state->map.exit_position.col = state->level_exit.col;
+  state->map.exit_position.row = state->level_exit.row;
 }
