201a8fd26230f60eeccf934234641c3af2300505
mouadse <mouadse@gmail.com>
2024-06-28 01:09:35 +0100

    Add functions to help dfs

--- dfs_utils.c
+++ dfs_utils.c
@@ -1,1 +1,23 @@
 #include "so_long.h"
+
+void set_2d_position(int position[2], int x, int y) {
+  position[0] = x;
+  position[1] = y;
+}
+
+int **create_visited_positions_tracker(t_game_state *state) {
+  int i;
+  int **tracker;
+
+  tracker = malloc(sizeof(int *) * state->map.cols * state->map.rows);
+  if (!tracker)
+    return (NULL);
+  i = 0;
+  while (i < state->map.cols * state->map.rows) {
+    tracker[i] = malloc(sizeof(int) * 2);
+    if (!tracker)
+      return (NULL);
+    i++;
+  }
+  return (tracker);
+}
