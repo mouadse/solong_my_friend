ff3191ec926ae2dc1382cf00d11c0d4e24622129
mouadse <mouadse@gmail.com>
2024-06-28 01:11:25 +0100

    finished the utils for dfs algo

--- dfs_utils.c
+++ dfs_utils.c
@@ -1,23 +1,39 @@
 #include "so_long.h"
 
-void set_2d_position(int position[2], int x, int y) {
-  position[0] = x;
-  position[1] = y;
+void	set_2d_position(int position[2], int x, int y)
+{
+	position[0] = x;
+	position[1] = y;
 }
 
-int **create_visited_positions_tracker(t_game_state *state) {
-  int i;
-  int **tracker;
+int	**create_visited_positions_tracker(t_game_state *state)
+{
+	int	i;
+	int	**tracker;
 
-  tracker = malloc(sizeof(int *) * state->map.cols * state->map.rows);
-  if (!tracker)
-    return (NULL);
-  i = 0;
-  while (i < state->map.cols * state->map.rows) {
-    tracker[i] = malloc(sizeof(int) * 2);
-    if (!tracker)
-      return (NULL);
-    i++;
-  }
-  return (tracker);
+	tracker = malloc(sizeof(int *) * state->map.cols * state->map.rows);
+	if (!tracker)
+		return (NULL);
+	i = 0;
+	while (i < state->map.cols * state->map.rows)
+	{
+		tracker[i] = malloc(sizeof(int) * 2);
+		if (!tracker)
+			return (NULL);
+		i++;
+	}
+	return (tracker);
+}
+
+void	deallocate_visited_positions_tracker(t_game_state *state, int **tracker)
+{
+	int	i;
+
+	i = 0;
+	while (i < state->map.cols * state->map.rows)
+	{
+		free(tracker[i]);
+		i++;
+	}
+	free(tracker);
 }
