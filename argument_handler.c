27d83d526e83ad47970265897e266fcb983aca87
mouadse <mouadse@gmail.com>
2024-07-01 21:49:30 +0100

    Added a file to handle args in our game

---
+++ argument_handler.c
@@ -0,0 +1,11 @@
+#include "so_long.h"
+
+int	ft_strcmp(const char *str1, const char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] == str2[i] && str1[i] != '\0' && str2[i] != '\0')
+		i++;
+	return (str1[i] - str2[i]);
+}
